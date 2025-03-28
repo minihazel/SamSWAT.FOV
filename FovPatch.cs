436ed9cd2d9ead675fc2b2d58ce68224897c27b4
minihazel <decyanidez@gmail.com>
2025-03-28 19:07:47 +0100

    Add project files.

---
+++ FovPatch.cs
@@ -0,0 +1,44 @@
+﻿using SPT.Reflection.Patching;
+using EFT.UI;
+using EFT.UI.Settings;
+using System;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Reflection;
+
+namespace SamSWAT.FOV
+{
+    public class FovPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return typeof(GameSettingsTab).GetMethod("Show");
+        }
+
+        [PatchPrefix]
+        private static void PatchPrefix(ref ReadOnlyCollection<int> ___readOnlyCollection_0)
+        {
+            if (FovPlugin.MaxFov.Value < FovPlugin.MinFov.Value)
+            {
+                FovPlugin.MinFov.Value = 50;
+                FovPlugin.MaxFov.Value = 75;
+            }
+
+            int rangeCount = FovPlugin.MaxFov.Value - FovPlugin.MinFov.Value + 1;
+            ___readOnlyCollection_0 = Array.AsReadOnly(Enumerable.Range(FovPlugin.MinFov.Value, rangeCount).ToArray());
+        }
+
+        [PatchPostfix]
+        private static void PatchPostfix(ref NumberSlider ____fov, GClass1053 ___gclass1053_0)
+        {
+            if (FovPlugin.MaxFov.Value < FovPlugin.MinFov.Value)
+            {
+                FovPlugin.MinFov.Value = 50;
+                FovPlugin.MaxFov.Value = 75;
+            }
+
+            int rangeCount = FovPlugin.MaxFov.Value - FovPlugin.MinFov.Value + 1;
+            ___readOnlyCollection_0 = Array.AsReadOnly(Enumerable.Range(FovPlugin.MinFov.Value, rangeCount).ToArray());
+        }
+    }
+}
---
+++ SettingsApplierPatch.cs
@@ -0,0 +1,24 @@
+﻿using SPT.Reflection.Patching;
+using SPT.Reflection.Utils;
+using System;
+using System.Linq;
+using System.Reflection;
+using UnityEngine;
+
+namespace SamSWAT.FOV
+{
+    public class SettingsApplierPatch : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            Type gclass911 = PatchConstants.EftTypes.Single(x => x.GetMethod("Clone") != null && x.GetField("NotificationTransportType") != null);
+            return gclass911.GetNestedTypes(PatchConstants.PrivateFlags)[0].GetMethod("method_0", PatchConstants.PrivateFlags);
+        }
+
+        [PatchPostfix]
+        public static void PatchPostfix(int x, ref int __result)
+        {
+            __result = Mathf.Clamp(x, FovPlugin.MinFov.Value, FovPlugin.MaxFov.Value);
+        }
+    }
+}
